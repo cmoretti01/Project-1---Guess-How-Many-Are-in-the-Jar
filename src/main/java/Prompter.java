28d288b9a8bcc3fc7fb7b0c10a8b4b5773ae92a8
Caio Moretti Marques <caiomoretti.compuway@gmail.com>
2017-02-06 16:01:33 -0200

    Matching requirements again.

--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -12,9 +12,9 @@ public class Prompter {
     public void playGame() {
         boolean isGuessValid = false;
 
-        System.out.printf("What type of item? ");
+        System.out.printf("What type of item? \n");
         itemInput = scanner.nextLine();
-        System.out.printf("What is the maximum amount of %s? ", itemInput);
+        System.out.printf("What is the maximum amount of %s? \n", itemInput);
         amount = scanner.nextInt();
 
         mJar = new Jar(itemInput, amount);
@@ -22,17 +22,15 @@ public class Prompter {
 
         do {
             do {
-                System.out.printf("**************************\n");
                 System.out.printf("How many %s are in the jar? Pick a number between 1 and %d: ",
                         mJar.getItem(), mJar.getMaxAmount());
                 guess = scanner.nextInt();
                 if (guess > mJar.getMaxAmount()) {
-                    System.out.printf("\nYour guess must be less than %s.", mJar.getItem());
+                    System.out.printf("Your guess must be less than %s.\n", mJar.getItem());
                 }
             } while (guess > mJar.getAmount()) ;
                 attempts++;
                 if (guess != mJar.getAmount()) {
-                    System.out.printf("Try again! ");
                     if (guess > mJar.getAmount()) {
                         System.out.printf("Your Guess is too high!\n");
                     } else {
@@ -42,6 +40,6 @@ public class Prompter {
                     isGuessValid = true;
                 }
             } while (!isGuessValid);
-        System.out.printf("\nCongrats! You got it in %d attempt(s)!", attempts);
+        System.out.printf("Congrats! You got it in %d attempt(s)!\n", attempts);
     }
 }

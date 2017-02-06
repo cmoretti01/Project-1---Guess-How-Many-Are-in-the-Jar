b18bad3b4bde90e98e23726f1d28c06948ab30c3
Caio Moretti Marques <caiomoretti.compuway@gmail.com>
2017-02-06 15:42:19 -0200

    Fixing the prompt issue.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,6 @@
 public class Game {
     public static void main(String[] args) {
         Prompter prompter = new Prompter();
-            prompter.promptForItemAndAmount();
+            prompter.playGame();
     }
 }
--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -9,41 +9,42 @@ public class Prompter {
     private int attempts;
 
     // Initial prompting to insert the item and fill the jar
-    public void promptForItemAndAmount () {
+    public void playGame() {
         boolean isGuessValid = false;
 
         System.out.printf("Guess How Many Are in the Jar - Welcome to the game!!\n");
         System.out.printf("-----------------------------------------------------\n");
         System.out.printf("What type item should be put in the jar? ");
         itemInput = scanner.nextLine();
-        System.out.printf("What is the maximum amount of %s? ",itemInput);
+        System.out.printf("What is the maximum amount of %s? ", itemInput);
         amount = scanner.nextInt();
 
         mJar = new Jar(itemInput, amount);
         mJar.fillJar(amount);
 
         do {
-            System.out.printf("**************************\n");
-            System.out.printf("How many %s are in the jar? Pick a number between 1 and %d: ",
-                    mJar.getItem(), mJar.getMaxAmount());
-            guess = scanner.nextInt();
-            attempts++;
-            while(guess > mJar.getMaxAmount()) {
-                System.out.printf("\nOps!!! The guess is larger than the maximum amount of %s.", mJar.getItem());
-                System.out.printf("\nPlease, give another guess: ");
+            do {
+                System.out.printf("**************************\n");
+                System.out.printf("How many %s are in the jar? Pick a number between 1 and %d: ",
+                        mJar.getItem(), mJar.getMaxAmount());
                 guess = scanner.nextInt();
-            }
-            if(guess != mJar.getAmount()) {
-                System.out.printf("Try again! ");
-                if(guess > mJar.getAmount()) {
-                    System.out.printf("Your Guess is too high!\n");
-                } else {
-                    System.out.printf("Your guess is too low!\n");
+                if (guess > mJar.getMaxAmount()) {
+                    System.out.printf("\nOps!!! The guess is larger than the maximum amount of %s.", mJar.getItem());
+                    System.out.printf("Please, give another guess.\n ");
                 }
-            } else {
-                isGuessValid = true;
-            }
-        } while(!isGuessValid);
+            }while (guess > mJar.getAmount()) ;
+                attempts++;
+                if (guess != mJar.getAmount()) {
+                    System.out.printf("Try again! ");
+                    if (guess > mJar.getAmount()) {
+                        System.out.printf("Your Guess is too high!\n");
+                    } else {
+                        System.out.printf("Your guess is too low!\n");
+                    }
+                } else {
+                    isGuessValid = true;
+                }
+            } while (!isGuessValid);
         System.out.printf("\nCongrats! You got it in %d attempt(s)!", attempts);
     }
 }
